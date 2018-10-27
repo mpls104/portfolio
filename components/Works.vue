c7d1b8e6d9f078f15d69e51d6b78c64f391377e0
Ryota Kusano <ryotakusano@imac.local>
2018-10-27 15:36:26 +0900

    add missing files

---
+++ components/Works.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="main-content">
+    <div class="container">
+      <h1 class="title has-text-centered">
+        Recent Works.
+      </h1>
+      <div class="columns is-multiline">
+        <div class="column is-half" v-for="work in works" :key="work">
+          <div class="card">
+           <header class="card-header">
+            <p class="card-header-title">
+              {{ work.title }}
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              {{ work.summary }}
+            </div>
+          </div>
+          <footer class="card-footer">
+            <nuxt-link :to="`work/${work.id}`"
+              class="card-footer-item" append>
+              Read More
+            </nuxt-link>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+  import works from '~/works.json'
+  export default {
+    name: 'works',
+    data () {
+      return { works }
+    }
+  }
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main-content">
+    <div class="container">
+      <h2 class="title is-2">About this website.</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'About'
+  }
+}
+</script>
